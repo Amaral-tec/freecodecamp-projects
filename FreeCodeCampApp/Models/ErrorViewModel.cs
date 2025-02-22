ec114a7f21495290237a0fa010043641ba52ec64
Amaral-tec <amaral_adm@hotmail.com>
2025-02-22 16:42:33 -0300

    Adicionar arquivos de projeto.

---
+++ FreeCodeCampApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeCodeCampApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
