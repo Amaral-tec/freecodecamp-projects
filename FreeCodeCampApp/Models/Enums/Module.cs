d5c0636e90f24f9d5c98188d371ced6acf0142e7
Amaral-tec <amaral_adm@hotmail.com>
2025-03-29 18:53:14 -0300

    Update index and add description field and section Display code in modals

---
+++ FreeCodeCampApp/Models/Enums/Module.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCodeCampApp.Models.Enums
+{
+    public enum Module
+    {
+        [Display(Name = "Responsive Web Design")]
+        ResponsiveWebDesign = 1,
+        [Display(Name = "JavaScript Algorithms and Data Structures")]
+        JavaScriptAlgorithmsDataStructures = 2,
+    }
+}
